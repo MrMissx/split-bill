abb61875aa46d87b04c2b0f5a89f11ced0bdd908
mrmissx <hi@mrmiss.dev>
2024-10-27 19:22:47 +0700

    add footer link

--- src/lib/layouts/Footer/Component.svelte
+++ src/lib/layouts/Footer/Component.svelte
@@ -5,6 +5,6 @@
 <div data-orientation="horizontal" role="none" class="h-[1px] w-full shrink-0 bg-border"></div>
 <footer class="container mx-auto max-w-screen-lg py-5">
 	<p class="text-center leading-7 text-muted-foreground">
-		© {year} Mr.Miss. All rights reserved.
+		© {year} <a class="hover:underline" href="http://mrmiss.dev">Mr.Miss</a>. All rights reserved.
 	</p>
 </footer>
